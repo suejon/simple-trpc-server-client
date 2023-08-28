3a9a0847dd0eafd4f0c45a2b0b53e2be57df1db9
jonothan sue <jonothansue@gmail.com>
2023-08-28 18:31:42 +0900

    add modules

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,16 @@
 import { publicProcedure, router } from "./trpc";
 import { string } from 'valibot'
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
+// import { z } from "zod";
 
 const appRouter = router({
-	userList: publicProcedure
-		.input(string())
-		.query(async ({ input }) => {
-			return await new Promise((resolve) => {
-				setTimeout(() => {
-					resolve([{ "id": 1, "name": "You said:" + input }])
-				})
-			})
-
-		})
+  userList: publicProcedure.input(string()).query(async ({ input }) => {
+    return await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve([{ id: 1, name: "You said:" + input }]);
+      });
+    });
+  }),
 });
 
 export type AppRouter = typeof appRouter;
